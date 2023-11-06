98e811fec1ce4b2c25b521841efc28454a3bdab3
mbolatianafesia <mbolatiana@gmail.com>
2023-11-06 16:52:38 +0300

    mis a jours front

--- src/template/Context.js
+++ src/template/Context.js
@@ -4,18 +4,26 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import './Context.css';
 
 export default function Context() {
-  const [buttonClicked, setButtonClicked] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(true);
 
   const handleDropdownItemClick = () => {
-    setButtonClicked(true);
+    setButtonClicked(!buttonClicked);
   };
-
-  // Déterminez la classe du bouton en fonction de buttonClicked
-  const buttonClassName = buttonClicked ? 'acc red-button' : 'acc';
 
   return (
   <>
     <header>
+      {
+        buttonClicked ? 
+        <div className="button-container">
+        <button onClick={handleDropdownItemClick}>
+        <img src="images/io.png" alt="Description de l'image" />
+        Click me
+      </button>
+      </div>
+      
+          :
+      
        <nav className={`navbar navbar-expand-md custom-navbar`}>
         <div id="logo">
           <Link className="link" to="/accueil">
@@ -42,32 +50,38 @@ export default function Context() {
           
           <div className="navbar-brand">
             <Dropdown>
-              <Dropdown.Toggle variant="" className={buttonClassName} style={{ color: "white", fontSize: "13px" }} id="dropdown-basic">
+              <Dropdown.Toggle variant=""  style={{ color: "white", fontSize: "13px" }} id="dropdown-basic">
                 CLASSE
               </Dropdown.Toggle>
 
               <Dropdown.Menu className="custom-dropdown-menu" style={{ textAlign: "left" }}>
-                <Dropdown.Item onClick={handleDropdownItemClick}><Link className="link acc" to="/classe">Niveau & parcours</Link></Dropdown.Item>
-                <Dropdown.Item onClick={handleDropdownItemClick}><Link className="link acc" to="/salle">Salle à étudier</Link></Dropdown.Item>
+                <Dropdown.Item onClick={handleDropdownItemClick} className='hov' ><Link className="link acc" to="/classe">Niveau & parcours</Link></Dropdown.Item>
+                <Dropdown.Item onClick={handleDropdownItemClick} className='hov'><Link className="link acc" to="/salle">Salle à étudier</Link></Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </div>
 
           <div className="navbar-brand">
             <Dropdown>
-              <Dropdown.Toggle variant="" className={buttonClassName} style={{ color: "white", fontSize: "13px" }} id="dropdown-basic">
+              <Dropdown.Toggle variant="" style={{ color: "white", fontSize: "13px" }} id="dropdown-basic">
                 ENSEIGNEMENTS
               </Dropdown.Toggle>
 
-              <Dropdown.Menu className="custom-dropdown-menu" style={{ textAlign: "left" }}>
-                <Dropdown.Item onClick={handleDropdownItemClick}><Link className="link acc" to="/semaine">Semaine</Link></Dropdown.Item>
-                <Dropdown.Item onClick={handleDropdownItemClick}><Link className="link acc" to="/disponible">Disponibilité</Link></Dropdown.Item>
-                <Dropdown.Item onClick={handleDropdownItemClick}><Link className="link acc" to="/enseignement">Assigner un cours</Link></Dropdown.Item>
+              <Dropdown.Menu className="custom-dropdown-menu" style={{ textAlign: "left" }} >
+                <Dropdown.Item onClick={handleDropdownItemClick} className='hov'><Link className="link acc" to="/semaine">Semaine</Link></Dropdown.Item>
+                <Dropdown.Item onClick={handleDropdownItemClick} className='hov'><Link className="link acc" to="/disponible">Disponibilité</Link></Dropdown.Item>
+                <Dropdown.Item onClick={handleDropdownItemClick} className='hov'><Link className="link acc" to="/enseignement">Assigner un cours</Link></Dropdown.Item>
               </Dropdown.Menu>
+              <button onClick={handleDropdownItemClick} >
+              <div className="butt-container">
+                    <img src={Image} alt="images/menu2.jpg"/>
+                </div>
+              </button>
             </Dropdown>
           </div>
         </div>
       </nav>
+      }
     </header>
     </>
   );
